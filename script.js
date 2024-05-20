9c922790c64275bc89111211488555d09e96546e
TerlogD <167808070+TerlogD@users.noreply.github.com>
2024-05-20 21:25:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 // Завдання 1: Цикл while
 let i = 1;
 while (i <= 10) {
-  console.log(i);
+  console.log("print - Loop while :", i);
   i++;
 }
 
 // Завдання 2: Цикл do...while
 let j = 10;
 do {
-  console.log(j);
+  console.log("print - Loop do...while :", j);
   j--;
 } while (j >= 1);
 
 // Завдання 3: Цикл for
 for (let k = 2; k <= 20; k += 2) {
-  console.log(k);
+  console.log("print - Loop for :",k);
 }
 
 // Завдання 4: Вкладений цикл
 for (let a = 1; a <= 5; a++) {
   for (let b = 1; b <= 10; b++) {
-    console.log(`${a} * ${b} = ${a * b}`);
+    console.log("print - Nested loop :",`${a} * ${b} = ${a * b}`);
   }
 }
 
@@ -29,7 +29,7 @@ for (let c = 1; c <= 10; c++) {
   if (c % 3 === 0) {
     continue;
   }
-  console.log(c);
+  console.log("print - Loop using continue :", c);
 }
 
 // Завдання 6: Цикл з використанням break
@@ -38,6 +38,7 @@ while (d <= 10) {
   if (d === 7) {
     break;
   }
-  console.log(d);
+  console.log("print - Loop using break :", d);
   d++;
 }
+
